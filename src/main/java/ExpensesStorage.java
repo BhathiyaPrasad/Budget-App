9167bb4d7686032ffc766f176a03342377832b7c
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-11-06 09:32:55 +0530

    Update

---
+++ src/main/java/ExpensesStorage.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.utill.ArrayList;
+
+public class ExpensesStorage {
+    private static final String FILENAME = "expenses.txt";
+
+    public static void savExpenses(ArrayList<Expenses>expenses) {
+        
+    }
+}
