b3f20b2a1cf0e6211d47e28b173793be81774f31
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-11-07 15:22:04 +0530

    Formatted ExpensesStorage

--- src/main/java/ExpensesStorage.java
+++ src/main/java/ExpensesStorage.java
@@ -4,28 +4,28 @@ import java.util.ArrayList;
 public class ExpensesStorage {
     private static final String FILENAME = "expenses.txt";
 
-    public static void saveExpenses(ArrayList<Expenses>expenses) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(FILENAME))){
-        for (Expenses e : expenses){
-            writer.println(e.getDate() + "," + e.getDescription() + "," + e.getAmount());
+    public static void saveExpenses(ArrayList<Expenses> expenses) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILENAME))) {
+            for (Expenses e : expenses) {
+                writer.println(e.getDate() + "," + e.getDescription() + "," + e.getAmount());
+            }
+        } catch (IOException e) {
+            System.out.println("Error Saving Expenses: " + e.getMessage());
         }
-    }catch (IOException e){
-         System.out.println("Error Saving Expenses: " + e.getMessage());
     }
-    }
+
     public static ArrayList<Expenses> loadExpenses() {
         ArrayList<Expenses> expenses = new ArrayList<>();
-        try(BufferedReader reader = new BufferedReader(new FileReader(FILENAME))){
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILENAME))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
-                expenses.add (new Expenses(parts[0],parts[1],Double.parseDouble(parts[2])));
+                expenses.add(new Expenses(parts[0], parts[1], Double.parseDouble(parts[2])));
             }
         } catch (IOException e) {
-            System.out.println("Error loading expenses: "+ e.getMessage());
+            System.out.println("Error loading expenses: " + e.getMessage());
 
         }
         return expenses;
     }
 }
-
