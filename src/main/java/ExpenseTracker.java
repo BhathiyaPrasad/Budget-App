ea0fcce1dc07c441a7c176647e2725e831d5ae3c
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-11-07 14:56:08 +0530

    created the main user interface

---
+++ src/main/java/ExpenseTracker.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExpenseTracker {
+    public static void main(String[] args){
+        Scanner.scanner new Scanner(System.in);
+        ArrayList<Expenses> expenses = ExpensesStorage.loadExpenses(); 
+
+        while (true) {
+            System.out.println("\nExpense Tracker Menu");
+            System.out.println();
+            
+        }
+    }
+}
