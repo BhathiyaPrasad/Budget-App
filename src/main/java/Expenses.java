a0f4df1cc66e8841a4bbc73b91b416cb687bbe84
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-11-07 15:22:36 +0530

    Formatted Expenses

--- src/main/java/Expenses.java
+++ src/main/java/Expenses.java
@@ -11,26 +11,27 @@ public class Expenses {
     private String date;
     private String description;
     private double amount;
-    
-    public Expenses(String date, String description, double amount){
+
+    public Expenses(String date, String description, double amount) {
         this.date = date;
         this.description = description;
-        this.amount = amount;}
-    
-    public String getDate(){
+        this.amount = amount;
+    }
+
+    public String getDate() {
         return date;
     }
-    public String getDescription(){
+
+    public String getDescription() {
         return description;
     }
-    public double amount(){
+
+    public double amount() {
         return amount;
     }
-    
-    @override
-    
-    public String toString(){
+
+    public String toString() {
         return "Expenses(date=" + date + ",description=" + " + description + , amount=" + amount + ")";
     }
-    
+
 }
