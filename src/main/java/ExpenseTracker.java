b0eaa840b747cac92553aed092195db963c8f553
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-11-07 15:03:25 +0530

    Creating

--- src/main/java/ExpenseTracker.java
+++ src/main/java/ExpenseTracker.java
@@ -8,7 +8,23 @@ public class ExpenseTracker {
 
         while (true) {
             System.out.println("\nExpense Tracker Menu");
-            System.out.println();
+            System.out.println("1. Add Expense");
+            System.out.println("2. View Expense");
+            System.out.println("3. Exit");
+            System.out.println("Enter your choice");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                System.out.print("Enter date (YYYY-MM-DD):");
+                String date = scanner.next();
+                scanner.nextline(); // consume newline after over
+                    
+                    break;
+            
+                default:
+                    break;
+            }
             
         }
     }
